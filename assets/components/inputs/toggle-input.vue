edb69a0c1674d619be4e6978cd2f58a6537c1524
Dan Howard <dh@madisonsolutions.co.uk>
2019-06-06 11:50:45 +0100

    Add toggle input

---
+++ assets/components/inputs/toggle-input.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="lcf-input lcf-input-toggle">
+        <input type="hidden" :name="name" :value="value ? 'true' : 'false'" />
+        <button @click="toggle" type="button">
+            <i v-if="value" class="fas fa-toggle-on"></i>
+            <i v-if="! value" class="fas fa-toggle-off"></i>
+            <span>{{ value ? trueLabel : falseLabel }}</span>
+        </button>
+    </div>
+</template>
+
+<script>
+import inputMixin from '../../input-mixin.js';
+export default {
+    mixins: [inputMixin],
+    computed: {
+        trueLabel: function() {
+            return this.setting('true_label', 'On');
+        },
+        falseLabel: function() {
+            return this.setting('false_label', 'Off');
+        }
+    },
+    methods: {
+        toggle: function() {
+            this.$emit('change', {key: this._key, value: ! this.value});
+        }
+    }
+};
+</script>
